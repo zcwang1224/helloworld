0dbbc85c124f9a39ed5a459c836c1c6881a9cd0e
zcwang <zcwang1224@gmail.com>
2018-11-21 18:31:42 +0800

    Initial commit.

---
+++ src/HelloWorldController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Zcwang\HelloWorld;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HelloWorldController extends Controller
+{
+    //
+    public function index()
+    {
+        $message = 'Hello World';
+        return view('HelloWorld::welcome', compact('message'));    	
+    }
+}
